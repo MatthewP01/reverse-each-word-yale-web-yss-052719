8d6e7ac94a332a48aab1f79d9228aaa146fed04d
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 22:35:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
+# def reverse_each_word(string)
+#   rev_string = string.split
+#   reversed_string = []
+#   rev_string.each do |rev|
+#     reversed = rev.reverse!
+#     reversed_string << reversed
+#   end
+#   return reversed_string.join(" ")
+# end
+
 def reverse_each_word(string)
   rev_string = string.split
-  reversed_string = []
-  rev_string.each do |rev|
-    reversed = rev.reverse!
-    reversed_string << reversed
-  end
-  return reversed_string.join(" ")
-end
